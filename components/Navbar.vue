25d48af86f04ad16140e007da159d4a68d296645
Jian Hao <vernonht@gmail.com>
2019-05-22 23:15:44 +0800

    :tada: first commit

---
+++ components/Navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex py-4 sticky top-0 border-b">
+  <div class="ml-auto mx-4">
+    <el-button size="small" plain>Login</el-button>
+  </div>
+</div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        activeIndex: '1',
+        activeIndex2: '1'
+      };
+    },
+    methods: {
+      handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style>
+</style>
